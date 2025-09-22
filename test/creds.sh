62c3787044f1ac0bbdc40003e7cb415c2835d5c0
dev-embedthis <mob-pub-18@emobrien.com>
2025-09-22 16:27:18 +1000

    TEST: revise unit tests

---
+++ test/creds.sh
@@ -0,0 +1,12 @@
+#
+#	creds.sh - Credentials for the updater
+#
+VERSION="YOUR-VERSION-HERE"
+DEVICE="YOUR-DEVICE-ID-HERE"
+
+#
+# 	Get these values from your Builder Product and Cloud 
+#
+PRODUCT="PRODUCT-ID-HERE"
+TOKEN="CLOUDAPI-TOKEN-HERE"
+ENDPOINT="CLOUD-ENDPOINT-HERE"
